90370aae29bb7d47f96a7a69f96d6ae7588b32f4
hacker6291 <98105494+hacker6291@users.noreply.github.com>
2024-01-08 15:02:11 +0530

    Update crack.py

--- crack.py
+++ crack.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import time #line:3
-from colorama import Fore ,Back ,Style ,init #line:4
+import time 
+from colorama import Fore ,Back ,Style ,init 
 init (autoreset =True )
-def startMessage ():#line:7
-    OO0O0OO0OOO0OO0O0 =input (Fore .YELLOW +"Enter Code To Unlock The Tool : ")#line:8
+def startMessage ():
+    OO0O0OO0OOO0OO0O0 =input (Fore .YELLOW +"Enter Code To Unlock The Tool : ")8
     OOOO0OO000OO0OOOO ="iloveu"
     if OOOO0OO000OO0OOOO !=OO0O0OO0OOO0OO0O0 :
         print (Fore .RED +'[X] Wrong Code')
